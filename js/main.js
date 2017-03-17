1f743c0d3e191634c4e1e886305a41d6ba92143f
Nikolay Solodukhin <Nikolay.Solodukhin@gmail.com>
2017-03-17 21:59:41 +0300

    Сделал сетку всего проекта кроме меню

--- js/main.js
+++ js/main.js
@@ -1,1 +1,21 @@
 'use strict';
+
+ymaps.ready(function() {
+  var myMap = new ymaps.Map("map-canvas", {
+      center: [59.935955, 30.321965],
+      zoom: 16
+    }, {
+      searchControlProvider: "yandex#search"
+    }),
+    myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
+      hintContent: "Собственный значок метки",
+      balloonContent: "HTML Academy"
+    }, {
+      iconLayout: "default#image",
+      iconImageHref: "img/icon-map-pin.svg",
+      iconImageSize: [78, 138],
+      iconImageOffset: [-45, -137]
+    });
+
+  myMap.geoObjects.add(myPlacemark);
+});
