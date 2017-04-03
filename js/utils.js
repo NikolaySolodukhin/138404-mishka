14eadce491cb7cff47316e3729cc7407bcb249c6
Nikolay Solodukhin <Nikolay.Solodukhin@gmail.com>
2017-04-04 02:39:27 +0300

    Доработки стайлгайда

--- js/utils.js
+++ js/utils.js
@@ -1,18 +1,37 @@
-"use strict";
+'use strict';
+
 window.utils = (function () {
 
-  let topMenuToggle = document.querySelector(".top-menu__toggle");
-  let mainNav = topMenuToggle.parentNode.querySelectorAll(".main-nav");
+  var topMenuToggle = document.querySelector('.top-menu__toggle');
+  var mainNav = topMenuToggle.parentNode.querySelectorAll('.main-nav');
+  var modalWindow = document.querySelector('.modal-window');
+  var order = document.querySelectorAll('.btn_product, .product__add');
 
   for (var i = 0; i < mainNav.length; i++) {
     mainNav[i].classList.remove('main-nav_nojs');
   }
 
-  topMenuToggle.addEventListener("click", function() {
+  topMenuToggle.addEventListener('click', function() {
     for (var i = 0; i < mainNav.length; i++) {
-      mainNav[i].classList.toggle("main-nav_show");
+      mainNav[i].classList.toggle('main-nav_show');
     }
-    topMenuToggle.classList.toggle("top-menu__toggle_close");
+    topMenuToggle.classList.toggle('top-menu__toggle_close');
   });
 
-})();;
+
+  for (var i = 0; i < order.length; i++) {
+    order[i].addEventListener('click', function(event) {
+      event.preventDefault();
+      modalWindow.classList.add('modal-window_show');
+    });
+  }
+
+  window.addEventListener('keydown', function(event) {
+    if (event.keyCode === 27) {
+      if (modalWindow.classList.contains('modal-window_show')) {
+        modalWindow.classList.remove('modal-window_show');
+      }
+    }
+  });
+
+})();
